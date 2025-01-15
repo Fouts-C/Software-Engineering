86a903eb7828e7dc62c6cb84b79d0ab63629dffb
Carson Fouts <carsonfouts@Carsons-MacBook-Air.local>
2025-01-15 12:31:14 -0500

    initial commit

---
+++ Class Notes/Git_VersionControl.java
@@ -0,0 +1,20 @@
+// Git & Version Control
+
+//Version Control System:
+// A tool that manages changes to your code. It tracks of every modification
+//collaborate with others & see who made specific changes EX: Git
+
+//git.init -> initializes an empty repository
+//start .git
+
+//Create File:
+// git add file1.txt
+// echo hello > file1.txt
+// echo hi > file2.txt
+// git add "Class Notes/Git_VersionControl.java"
+// git add "Class Notes/"
+
+
+// git status 
+
+//git commit -m "initial commit"
