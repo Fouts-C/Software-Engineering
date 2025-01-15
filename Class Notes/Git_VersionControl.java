37419313e2bd4381281fa8e9ff67df3212b022af
Carson Fouts <carsonfouts@Carsons-MacBook-Air.local>
2025-01-15 12:56:30 -0500

    updated notes

--- Class Notes/Git_VersionControl.java
+++ Class Notes/Git_VersionControl.java
@@ -1,20 +1,29 @@
-// Git & Version Control
+/*  
 
-//Version Control System:
-// A tool that manages changes to your code. It tracks of every modification
-//collaborate with others & see who made specific changes EX: Git
+*Git & Version Control*
 
-//git.init -> initializes an empty repository
-//start .git
+- Version Control System:
+    -A tool that manages changes to your code. It tracks of every modification
+    -collaborate with others & see who made specific changes EX: Git
 
-//Create File:
-// git add file1.txt
-// echo hello > file1.txt
-// echo hi > file2.txt
-// git add "Class Notes/Git_VersionControl.java"
-// git add "Class Notes/"
+Git commands:
 
+git.init -> initializes an empty repository
+start .git (not used???)
 
-// git status 
+Create File:
+echo hello > file1.txt
+echo hi > file2.txt
 
-//git commit -m "initial commit"
+Adding Files:
+git add "Class Notes/Git_VersionControl.java"
+git add "Class Notes/"
+git add file1.txt
+
+Git Steps:
+git status 
+git commit -m "initial commit"
+git branch - M main
+git push - u origin remote
+
+*/
