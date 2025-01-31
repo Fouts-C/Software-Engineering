2916cce8faad1f0219cd6efebc97b723e575d866
Carson Fouts <carsonfouts@Carsons-MacBook-Air.local>
2025-01-31 12:49:20 -0500

    Added notes on XP

--- Class Notes/AgileSoftwareDev.java
+++ Class Notes/AgileSoftwareDev.java
@@ -23,7 +23,7 @@ Analysis -> Design -> Implementation -> Testing -> Deployment -> Maintainance
 Modern software development processes:
 - Scrum, XP, Kanban
 
-Scrum:
+    *Scrum*
 - Emphasizes project manageement, what work is getting done, 
 - A scrum team works in Sprint 
 - A sprint is a time box of one months or less
@@ -43,8 +43,47 @@ Sprint Review:
 Sprint Retrospective:
 - The team reflects on what went well, what went bad, and what could be improved
 
+Scrum Values:
+- Courage: Team members have courage to take on challenges
+- Respect: Team members have mutual respect
+- Openness: Team members know what each other are working on
+- Focus: Team members are focused on sprint goal
+- Commitment: Every person on the team is committed to delivering the most valuable product
+
+Scrum Principles: 
+- Transparency: processs visibility, shared code base repo, daily scrum
+- Inspection: Deviation from expectation prevented by frequent inspection. Sprint review & retrospective
+- Adaptation: Sprint retrospective
 
 
+    *XP*
+- XP is a lightweight methodology for small/medium sized teams developing software in the face of rapidly changing requirement
+- Small: Release soon & get feedback, adapt quickly to possible change, avoid working too long without customer validation
+- Refactioning: We may need to change our design. We take the code, break into pieces, addcode, make it simple
 
+XP Values & Principle:
+- Most importatn value is communication 
+- XP creates right communication flowing & uses the following practices:
+  - pair programming: All production code is written with two people looking at one machine (Don't interfere with each others
+                      code/playing a different role of programming & strategy) look at the code to ensure it works, requires
+                      testing. Make it simple when possible
 
+  - user stories
+  - customer involvement
+
+- Simplicity: Take the simpliest thing that works (Simple not stupid)
+- Feedback: Take feedback in differet level (Developer write test cases & gets immediate feedback. Developer estimates
+            new stories, take feedback based on that proceeds & then take feedback)
+- Courage: Throw out the code if it doesn't work properly, add new feature if it works better than before
+
+Incremental Planning: 
+  Select user stories for release -> break stories into tasks -> plan release -> develop; integrate test -> release software ->
+  evaluate system & iteration -> repeat
+
+Continuation Integration: 
+
+            programming <-> local test -> integrate -> system test -> programming 
+
+On site customer: 
+- The customer is an actual member of the team (sits with team & brings requirements)
  */
