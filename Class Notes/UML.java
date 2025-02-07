8f997bee3edf9ec5d6470818db3de435ac084b58
Carson Fouts <carsonfouts@Carsons-MacBook-Air.local>
2025-02-07 14:19:31 -0500

    Added notes on UML

---
+++ Class Notes/UML.java
@@ -0,0 +1,48 @@
+/*
+        * UML - Unified Modeling Language *
+- A set of graphical notation techniques to create visual models of software system
+- Used for specifying, visualizing, constructing, documenting, business modeling, & communications
+
+Types of UML:
+
+- Structural Diagram
+    - Class Diagram
+    - Object Diagram
+
+- Functional Diagram
+    - Use Case Diagram:
+    - Actors
+        - External Entities
+        - Not part of the system
+        - Can be primary or secondary
+        - Primary initiates (Left) & Secondary Reacts (Right)
+    - Use Cases
+        - Major system components
+        - Nmae indicates purpose
+        - Starts with verb
+    - Interaction (lines)
+        - Indicates which use cases each actors interact with
+    Ex: 
+    Login to App -> Check Balance -> Transfer Funds
+    User (Primary Actor) has access to all functions while Bank (Secondary Actor) accesses check balance & transfer funds
+
+- Behavioral
+    - Sequence Diagram
+    - Activity Diagram 
+    - State Diagram
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+ */
